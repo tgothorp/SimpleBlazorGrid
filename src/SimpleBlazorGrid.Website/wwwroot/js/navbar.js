40900ead8b4a0b63b69ea28946bf549e67c05993
Tom Gothorp <tom.gothorp@hotmail.com>
2023-09-26 14:00:03 +0100

    Collapsible docs menu

--- src/SimpleBlazorGrid.Website/wwwroot/js/navbar.js
+++ src/SimpleBlazorGrid.Website/wwwroot/js/navbar.js
@@ -6,3 +6,22 @@ function toggleMobileMenu() {
         x.className = "navbar";
     }
 }
+
+function toggleDocumentationMenu() {
+    const sidebar = document.getElementById("sidebar");
+    const sidebarToggle = document.getElementById("sidebar-toggle");
+    const content = document.getElementById("documentation");
+    
+    if (sidebar.classList.contains('sidebar-collapsed'))
+    {
+        sidebar.classList.remove('sidebar-collapsed');
+        sidebarToggle.classList.remove('sidebar-toggle-collapsed');
+        content.classList.remove('documentation-wrapper-collapsed');
+    }
+    else
+    {
+        sidebar.classList.add('sidebar-collapsed');
+        sidebarToggle.classList.add('sidebar-toggle-collapsed');
+        content.classList.add('documentation-wrapper-collapsed');
+    }
+}
