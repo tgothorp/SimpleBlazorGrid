75c0e37755397aeaeca1d740fb9ab6f16d89502d
Tom Gothorp <tom.gothorp@hotmail.com>
2023-10-31 13:38:41 +0000

    Start POC on editable rows

---
+++ src/SimpleBlazorGrid.Core/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace SimpleBlazorGrid.Extensions;
+
+public static class ExpressionExtensions
+{
+    public static PropertyInfo GetPropertyInfo<T, P>(this Expression<Func<T, P>> accessor)
+    {
+        var member = accessor.Body as MemberExpression;
+        var property = member.Member as PropertyInfo;
+
+        return property;
+    }
+}
