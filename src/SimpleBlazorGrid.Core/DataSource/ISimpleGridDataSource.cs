f498687294c954b8e71cdbde2d3567511a040476
tgothorp <tgothorp@users.noreply.github.com>
2023-11-02 16:52:59 +0000

    Data source implementation

--- src/SimpleBlazorGrid.Core/DataSource/ISimpleGridDataSource.cs
+++ src/SimpleBlazorGrid.Core/DataSource/ISimpleGridDataSource.cs
@@ -6,5 +6,6 @@ namespace SimpleBlazorGrid.DataSource
     public interface ISimpleGridDataSource<T>
     {
         public Task<TableState<T>> LoadItems(TableState<T> tableState, CancellationToken cancellationToken = default);
+        public Task<TableState<T>> UpdateItem(TableState<T> tableState, T item, CancellationToken cancellationToken = default);
     }
 }
